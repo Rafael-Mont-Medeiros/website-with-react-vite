2b8c47f0ee30c60ad5bac2b440ae293898449bad
Rafael-Mont-Medeiros <rafak1212@gmail.com>
2025-02-06 20:17:25 -0300

    desenvolvento  menu "hamburguer".

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,28 +3,33 @@ import style from "./Header.module.css"
 import { useState } from "react"
 
 export default function Header() {
-    const[menuStart, setMenuStart] = useState(false)
-    const toggleMenu = ()=>{
-        setMenuStart(!menuStart)
+    const [showMenu, setShowMenu] = useState(false)
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
     }
+
     return (
         <>
-            <header className={`${style.headerContainer} ${menuStart ? style.active : ''}`}>
+            <header className={style.headerContainer}>
                 <Link to="/" className={style.title}>RafaelMedeiros.dev</Link>
 
-                <nav className={style.disable}>
+                <nav className={style.menuHamburguer} >
                     <Link to="/">Home</Link>
                     <Link to="sobre">Sobre</Link>
                     <Link to="projetos">Projetos</Link>
                     <Link to="contatos">Contatos</Link>
                 </nav>
 
-                <div onClick={toggleMenu}  className={style.menuHamburguer}>
+                <div
+                    onClick={toggleMenu}
+                    className={style.menu}
+                >
                     <span className={`${style.line} ${style.line1}`}></span>
                     <span className={`${style.line} ${style.line2}`}></span>
                     <span className={`${style.line} ${style.line3}`}></span>
                 </div>
             </header>
         </>
+
     )
 }
