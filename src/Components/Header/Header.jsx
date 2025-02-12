f6f840d6115552a033de641ca56edd929a9efba0
Rafael-Mont-Medeiros <rafak1212@gmail.com>
2025-02-12 08:48:26 -0300

    faltando adicionar animações fluidas no menu "hamburguer".

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,42 +4,36 @@ import { useState } from "react"
 
 export default function Header() {
     const [showMenu, setShowMenu] = useState(false)
-    const [animation, setAnimation] = useState(false)
 
     const toggleMenu = () => {
-        setAnimation(!animation)
         setShowMenu(!showMenu)
     }
 
     return (
-        <>
-            <header className={`${style.headerContainer}`}>
-                <Link
-                    to="/"
-                    className={style.title}>RafaelMedeiros.dev
-                </Link>
+        <header className={`${style.headerContainer}`}>
+            <Link
+                to="/" className={style.title}>
+                RafaelMedeiros.dev
+            </Link>
 
-                <nav
-                    className={`{${style.menuHamburguer}
-                    ${showMenu ? style.show : style.menuHamburguer}`}
-                >
-                    <Link to="/">Home</Link>
-                    <Link to="sobre">Sobre</Link>
-                    <Link to="projetos">Projetos</Link>
-                    <Link to="contatos">Contatos</Link>
-                </nav>
+            <nav
+                className={`{${style.menuHamburguer} ${showMenu ? style.show : style.menuHamburguer}`}
+            >
+                <Link to="/">Home</Link>
+                <Link to="sobre">Sobre</Link>
+                <Link to="projetos">Projetos</Link>
+                <Link to="contatos">Contatos</Link>
+            </nav>
 
-                <div
-                    onClick={toggleMenu}
-                    className={`${style.menu} 
-                    ${animation ? style.active : style.menu}`}
-                >
-                    <span className={`${style.line} ${style.line1}`}></span>
-                    <span className={`${style.line} ${style.line2}`}></span>
-                    <span className={`${style.line} ${style.line3}`}></span>
-                </div>
-            </header>
-        </>
+            <div
+                onClick={toggleMenu}
+                className={`${style.menu} ${showMenu ? style.active : ""}`}
+            >
+                <span className={`${style.line} ${style.line1}`}></span>
+                <span className={`${style.line} ${style.line2}`}></span>
+                <span className={`${style.line} ${style.line3}`}></span>
+            </div>
+        </header>
 
     )
 }
